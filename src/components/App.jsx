0c9c0814021e2402373139b2843075921435c839
Sasha.Solo <sasha.solomannik@gmail.com>
2022-12-29 20:00:26 +0200

    add APP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+import { AppBar } from './AppBar/AppBar';
+import { Home } from '../pages/Home/Home';
+import { Movies } from '../pages/Movies/Movies';
+import { NotFound } from 'pages/NotFound/NotFound';
+// import { StyledLink } from './App.Styled';
+
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Review = lazy(() => import('./Reviews/Reviews'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <AppBar />
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/movies/:movieID" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Review />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
