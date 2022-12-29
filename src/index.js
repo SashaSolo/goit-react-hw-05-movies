3e69117a92fca1a8ac3c87b427027fd64bc92804
Sasha.Solo <sasha.solomannik@gmail.com>
2022-12-29 19:59:54 +0200

    base

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { App } from 'components/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <React.StrictMode>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+        <GlobalStyle />
+      </Suspense>
+    </React.StrictMode>
+  </BrowserRouter>
 );
