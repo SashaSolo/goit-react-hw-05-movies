d2531157e7e4908d7cbba252904aef45409796fd
Sasha.Solo <sasha.solomannik@gmail.com>
2022-12-29 21:56:35 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 
-import GlobalStyle from './components/GlobalStyle';
+import { GlobalStyle } from './components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/goit-react-hw-05-movies/">
